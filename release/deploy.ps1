a8bd7b023b99bed2abc0329d43d2bcb292241895
Enrique Gonzalez <9368226+E-G-C@users.noreply.github.com>
2022-05-28 14:49:43 -0400

    updated confog names

---
+++ release/deploy.ps1
@@ -0,0 +1,19 @@
+$SDDC_NUMBER = 5
+
+$NUMBER_OF_LABS = 10
+
+
+Write-Output  '########### Generating new labs Ids #########################'
+.\labdeploy -id -d .\labs\
+
+Write-Output '############  Bootstrapping the Config #######################'
+.\labdeploy.exe -bootstrap -config .\configs\SDDC$SDDC_NUMBER-config.yml | Tee-Object .\logs\bootstrap.log 
+
+
+
+for (($i = 1); $i -lt $NUMBER_OF_LABS + 1; $i++) {
+ 
+    Write-Output "############# CREATING LAB#$i #############"
+   
+    .\labdeploy.exe -config .\configs\SDDC$SDDC_NUMBER-config.yml -lab .\labs\SDDC$SDDC_NUMBER\lab$i.yml | Tee-Object .\logs\lab$i.log
+}
